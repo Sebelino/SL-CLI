4dcd499de553af6670e7cddade5d3c2b2415cc64
Sebastian Olsson <sebelino@hotmail.com>
2014-07-12 08:56:14 +0200

    time -> positional argument.

--- sl-reseplanerare.py
+++ sl-reseplanerare.py
@@ -7,7 +7,7 @@ api = API(r'https://api.trafiklab.se/sl/reseplanerare.json',{
     'key' : {'required':True,'description':"Din API-nyckel."},
     's' : {'required':True,'description':"Namn eller ID för startpunkten."},
     'z' : {'required':True,'domain':bool,'description':"Namn eller ID för destinationen."},
-    'time' : {'required':False,'domain':str,'default':10,'description':"Tidpunkt."},
+    'time' : {'required':True,'domain':str,'description':"Tidpunkt."},
     'timesel' : {'required':False,'domain':set(["arrive","depart"]),'default':"depart",'description':"Betecknar tidpunkten ankomst eller avgång?"},
     'lang' : {'required':False,'domain':set(["sv","en"]),'default':"sv",'description':"Språket som resultaten presenteras i."},
 })
