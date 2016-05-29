5409c2b0b8f84469a3aecc57d77fcf0ecf4d1558
Sebastian Olsson <sebelino7@gmail.com>
2016-05-29 21:05:49 +0200

    sensitive.xml is now found by a referencing XML file, locations.xml.

--- slcli/keyreader.py
+++ slcli/keyreader.py
@@ -3,7 +3,13 @@
 
 import argparse
 import os
+import os.path
+import errno
 import xml.etree.ElementTree as ET
+from pkg_resources import resource_stream
+
+# XML file storing one or more paths to the XML file containing the API keys
+LOCATOR_FILE = resource_stream('slcli.resources', 'locations.xml')
 
 
 def get_keys():
@@ -11,23 +17,40 @@ def get_keys():
     keys["platsuppslag"] = os.environ.get("PLATSUPPSLAG")
     keys["reseplanerare2"] = os.environ.get("RESEPLANERARE")
     if all(k is None for k in keys.values()):
-        return read_keys()
+        return find_keys()
     else:
         print("Found keys in environment variables. Using those...")
         return keys
 
 
-def read_keys():
-    xmlfile = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           'sensitive.xml')
-    if not os.path.isfile(xmlfile):
-        raise Exception("Filen {} saknas.".format(xmlfile))
-    tree = ET.parse(xmlfile)
+def find_keys():
+    """ Reads API keys from the XML files referenced in the param xml """
+    locator_xml = LOCATOR_FILE.read().decode('utf8')
+    root = ET.fromstring(locator_xml)
+    for child in root:
+        if child.tag == 'path':
+            try:
+                path = child.text
+                dirpath = os.path.dirname(LOCATOR_FILE.name)
+                fullpath = os.path.join(dirpath, path)
+                return read_keys(fullpath)
+            except OSError as e:
+                if e.errno == errno.ENOENT:
+                    continue
+                else:
+                    raise
+    exceptionfmt = 'None of the files referenced by {} were found.'
+    raise Exception(exceptionfmt.format(LOCATOR_FILE.name))
+
+
+def read_keys(path):
+    """ Reads API keys from the XML file specified by param path """
+    tree = ET.parse(path)
     root = tree.getroot()
     return dict([(key.attrib['name'], key.text) for key in root.iter('key')])
 
 if __name__ == '__main__':
-    keys = read_keys()
+    keys = get_keys()
     parser = argparse.ArgumentParser()
     parser.add_argument('api', choices=list(keys))
     args = parser.parse_args()
