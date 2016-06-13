fe7a46eb7e127d13bcc40c576e42bfe0a9d71a8b
Sebastian Olsson <sebelino7@gmail.com>
2016-06-13 14:53:43 +0200

    Added test case for read_keys.

---
+++ slcli/test/test_keyreader.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from nose.tools import assert_equal
+import os
+from os.path import dirname, realpath, join
+from ..keyreader import read_keys
+
+
+class TestReadKeys:
+    @classmethod
+    def setup_class(cls):
+        xmlcontents = """
+<?xml version='1.0' encoding='utf-8'?>
+<root>
+    <key name="platsuppslag"  >abcdef</key>
+    <key name="reseplanerare2">123456789</key>
+</root>
+        """.strip()
+        cls.path = join(dirname(realpath(__file__)), "test.xml")
+        outfile = open(cls.path, 'w')
+        outfile.write(xmlcontents)
+
+    def test_read_keys(self):
+        returned = read_keys(self.path)
+        assert_equal(returned["platsuppslag"], "abcdef")
+        assert_equal(returned["reseplanerare2"], "123456789")
+
+    @classmethod
+    def teardown_class(cls):
+        os.remove(cls.path)
+        cls.path
