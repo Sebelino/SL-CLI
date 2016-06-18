b1d5b2b34b5704da8d00601f6ee6bddd631184e3
Sebastian Olsson <sebelino7@gmail.com>
2016-06-18 15:32:28 +0200

    Added unfinished test suite for api.py.

---
+++ slcli/test/test_api.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from nose.tools import raises, assert_equal
+from ..apis.api import requestURL
+import socket
+
+
+class TestRequestURL:
+    @classmethod
+    def setup_class(cls):
+        cls.oldsocket = socket.socket
+
+    @staticmethod
+    def guard(*args, **kwargs):
+        raise Exception("Internet access intentionally blocked.")
+
+    def test_empty_json(self):
+        branch = "devel"
+        url = ("https://github.com/Sebelino/SL-CLI/tree/{}/slcli/test/assets/js"
+               "on/empty.json").format(branch)
+        returned = requestURL(url)
+        expected = {}
+        assert_equal(returned, expected)
+
+    def test_nontrivial_json(self):
+        pass
+
+    def test_non_json(self):
+        pass
+
+    @raises(Exception)
+    def test_closed_socket(self):
+        socket.socket = self.guard
+        requestURL(self.url)
+
+    def teardown(self):
+        """ Restore internet connection in case it was blocked """
+        socket.socket = self.oldsocket
